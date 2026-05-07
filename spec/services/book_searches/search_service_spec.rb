4bbd49143541ba9354806224c14abc201c5907f6
ShinJaehun <gunsmoke.shin@gmail.com>
2026-05-07 22:17:35 +0900

    책 썸네일 표시 방식 정리

--- spec/services/book_searches/search_service_spec.rb
+++ spec/services/book_searches/search_service_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe BookSearches::SearchService do
             "title" => "미움받을 용기",
             "authors" => [ "기시미 이치로", "고가 후미타케" ],
             "publisher" => "인플루엔셜",
-            "thumbnail" => "",
+            "thumbnail" => "https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Fexample.com%2Fcover.jpg",
             "isbn" => "8996991341 9788996991342",
             "contents" => "소개",
             "url" => "https://example.com"
@@ -22,7 +22,7 @@ RSpec.describe BookSearches::SearchService do
     result = described_class.call(query: "미움받을 용기", page: 2)
 
     expect(result[:results].first[:authors_text]).to eq("기시미 이치로, 고가 후미타케")
-    expect(result[:results].first[:thumbnail]).to be_nil
+    expect(result[:results].first[:thumbnail]).to eq("https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Fexample.com%2Fcover.jpg")
     expect(result[:meta][:total_count]).to eq(1)
   end
 end
